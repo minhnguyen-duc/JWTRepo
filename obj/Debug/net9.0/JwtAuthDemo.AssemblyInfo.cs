f66d93e0f71c3036f8bcf632d1f6daf7b7bddc2d
nmh1003 <riojan6889@gmail.com>
2026-01-31 15:59:17 +0700

    edtit file name

--- obj/Debug/net9.0/JwtAuthDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/JwtAuthDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43102830a1dec3d2a696d94bcf7b16a2c5ea2038")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
